50bd8ef53a4cca6eaddd5f4ea86587c5bec9e099
FranciscoJSB12 <javif8@gmail.com>
2024-05-04 17:29:50 -0400

    feat: Create counter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView } from 'react-native';
-import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen';
+import { CounterScreen } from './src/presentation/screens/CounterScreen';
+//import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen';
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <HelloWorldScreen />
+      {/*<HelloWorldScreen />*/}
+      <CounterScreen />
     </SafeAreaView>
   );
 };
